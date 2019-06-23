cf32e94af47cb6ca5771a40290d48b7a17aa68c6
appseed246 <guitar.telecaster2017@gmail.com>
2019-06-23 16:12:58 +0900

    cache削除

---
+++ 04/mult/mult.asm
@@ -0,0 +1,35 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Mult.asm
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+
+// Put your code here.
+    @result
+    M=0
+    @1
+    D=M
+    @i
+    M=D
+(LOOP)
+    @i
+    D=M
+    @END
+    D;JEQ
+    @0
+    D=M
+    @result
+    M=M+D
+    @i
+    M=M-1
+    @LOOP
+    0;JMP
+(END)
+    @result
+    D=M
+    @2
+    M=D
+    @END
+    0;JMP
